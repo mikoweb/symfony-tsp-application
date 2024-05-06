6e1326277ebb4984fcbc9632529cf948a16cf5aa
Rafał Mikołajun <root@rmweb.pl>
2024-05-06 18:40:58 +0200

    Added getDistance method to DistanceMap

--- src/Shared/Domain/DistanceMap.php
+++ src/Shared/Domain/DistanceMap.php
@@ -5,6 +5,7 @@ namespace App\Shared\Domain;
 use PhpUnitConversion\Unit\Length\KiloMeter;
 use Ramsey\Collection\Map\AbstractTypedMap;
 use Ramsey\Collection\Map\TypedMap;
+use UnexpectedValueException;
 
 /**
  * @extends AbstractTypedMap<string, TypedMap<string, KiloMeter>>
@@ -19,5 +20,23 @@ class DistanceMap extends AbstractTypedMap
     public function getValueType(): string
     {
         return TypedMap::class;
+    }
+
+    public function getDistance(string $a, string $b): KiloMeter
+    {
+        $this->checkDistanceExists($a, $b);
+
+        return $this->get($a)->get($b);
+    }
+
+    private function checkDistanceExists(string $a, string $b): void
+    {
+        if (!$this->containsKey($a)) {
+            throw new UnexpectedValueException(sprintf('Not found distance row %s', $a));
+        }
+
+        if (!$this->get($a)->containsKey($b)) {
+            throw new UnexpectedValueException(sprintf('Not found distance cell %s : %s', $a, $b));
+        }
     }
 }
